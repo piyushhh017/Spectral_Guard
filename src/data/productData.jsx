bef66ca2493ea190897b71eaf8e6d443e6dd794c
piyush0004 <154751901+piyushhh017@users.noreply.github.com>
2025-01-14 11:22:01 +0530

    Add files via upload

---
+++ src/data/productData.jsx
@@ -0,0 +1,77 @@
+const productData = [
+    {
+        'id':1,
+        'type':'brace',
+        'name':'Watch Blue',
+        'price':55,
+        'image':"/images/bracelets/b1.png",
+        'description':'The "Spectral Guard" watch with a blue bracelet is a stylish and durable timepiece. It features a blue rubber strap, luminous Arabic numerals, and a polished stainless steel case. This watch is also water-resistant up to 100 meters, making it perfect for various activities.'
+    },
+    {
+        'id':2,
+        'type':'brace',
+        'name':'Band Blue',
+        'price':50,
+        'image':"/images/bracelets/b2.png",
+        'description':'The "Spectral Guard" band with a blue bracelet is a sleek and versatile accessory. It features a comfortable blue rubber strap, perfect for everyday wear. The band is designed with a minimalist aesthetic, making it a stylish addition to any outfit. Its durable construction ensures longevity, while the subtle design elements add a touch of sophistication.'
+    },
+    {
+        'id':3,
+        'type':'brace',
+        'name':'Lucky Blue',
+        'price':53,
+        'image':"/images/bracelets/b3.png",
+        'description':'The "Lucky Blue" band with a blue bracelet is a trendy and versatile accessory. It features a comfortable blue rubber strap, perfect for daily wear. The band is designed with a sleek and modern aesthetic, making it a stylish addition to any outfit. Its durable construction ensures longevity, while the subtle design elements add a touch of elegance.'
+    },
+    {
+        'id':4,
+        'type':'pend',
+        'name':'Fortune Blue',
+        'price':85,
+        'image':"/images/pendants/p1.png",
+        'description':'The "Fortune Blue" pendant is a stunning and elegant accessory. It features a beautiful blue gemstone set in a polished silver frame. The pendant is designed with a modern and sophisticated aesthetic, making it a perfect addition to any outfit. Its durable construction ensures longevity, while the intricate design elements add a touch of luxury.'
+    },
+    {
+        'id':5,
+        'type':'pend',
+        'name':'Mystic Pendant',
+        'price':83,
+        'image':"/images/pendants/p2.png",
+        'description':'The "Mystic Pendant" is a captivating and elegant accessory. It features a mesmerizing blue gemstone set in a polished silver frame. The pendant is designed with a modern and sophisticated aesthetic, making it a perfect addition to any outfit. Its durable construction ensures longevity, while the unique design elements add a touch of mystique.'
+    },
+    {
+        'id':6,
+        'type':'pend',
+        'name':'Eye Curve Pendant',
+        'price':97,
+        'image':"/images/pendants/p3.png",
+        'description':'The "Eye Curve" pendant is a unique and stylish accessory. It features a gracefully curved design with a captivating blue gemstone set in a polished silver frame. The pendant is modern and sophisticated aesthetic makes it a perfect addition to any outfit. Its durable construction ensures longevity, while the distinctive design elements add a touch of elegance.'
+    },
+    {
+        'id':7,
+        'type':'hom',
+        'name':'Fortune Blue Soul',
+        'price':185,
+        'image':"/images/Home decors/h1.png",
+        'description':'The "Fortune Blue Soul" home decor piece is a captivating and elegant addition to any space. It features a stunning blue design that exudes tranquility and sophistication. The decor piece is crafted with high-quality materials, ensuring durability and longevity. Its unique design elements add a touch of luxury and charm, making it a perfect centerpiece for your home.'
+    },
+    {
+        'id':8,
+        'type':'hom',
+        'name':'Mystic Hanger',
+        'price':103,
+        'image':"/images/Home decors/h2.png",
+        'description':'The "Mystic Hanger" home decor piece is a unique and stylish hanger. It features a captivating design with intricate details that add a touch of elegance and mystique. The hanger is crafted with high-quality materials, ensuring durability and longevity. Its modern aesthetic makes it a perfect centerpiece for your home, adding both functionality and charm.'
+    },
+    {
+        'id':9,
+        'type':'hom',
+        'name':'Hand Eye Protector',
+        'price':177,
+        'image':"/images/Home decors/h4.png",
+        'description':'The "Hand Eye Protector" is a versatile and essential safety accessory. It features a combination of hand and eye protection, designed to ensure maximum safety in various environments. The protector includes durable gloves and safety goggles, both crafted with high-quality materials to provide comfort and longevity. Its ergonomic design ensures a secure fit, while the protective elements shield against potential hazards, making it an ideal choice for industrial and everyday use.'
+    },
+  ];
+  
+  export default productData;
+  
